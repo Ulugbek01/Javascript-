7ac322f44215ba850e6497678ba6e8a4e2e6cf01
Ulugbek01 <xyz207820@gmail.com>
2022-03-05 21:49:28 +0500

    Prue Javascript

---
+++ main.js
@@ -0,0 +1,58 @@
+"use strict";
+// let id = Symbol("id");
+// const alsoHuge = BigInt(9007199254740991);
+
+// let age = 23;
+// let str = `My age is ${age}`;
+
+// let str1 = "My name is Ulugbek";
+// let str2 = "My name is Ulugbek";
+
+// let symbol = Symbol("id");
+// let bigInt = BigInt("23232432434543");
+// let num1 = 12;
+// let num2 = 12;
+
+// let obj1 = {
+//   name: "Ulugbek",
+// };
+
+// let obj2 = {
+//   name: "Ulugbek",
+// };
+// // (obj.name = "Ali"),
+// console.log(obj1.name === obj2.name);
+
+// console.log(typeof number);
+// let age = 23;
+// console.log(Boolean("false"));
+
+/* Function declaration */
+// function kvadrat(son1, son2) {
+//   return son1 * son2;
+// }
+// console.log(kvadrat(12, 3));
+
+/* Function expression */
+
+// const kvadrat = function (son1, son2) {
+//   return son1 + son2;
+// };
+// console.log(kvadrat(1, 4));
+
+/* Arrow function */
+
+// const kvadrat = (son) => console.log(son * son);
+
+// console.log(kvadrat(3));
+// kvadrat(4);
+
+// const print = () => console.log("Printing...");
+
+// print();
+
+// const languages = ["js", "java", "go"];
+
+const languages = new Array("js", "java", "go");
+
+console.log(languages);
